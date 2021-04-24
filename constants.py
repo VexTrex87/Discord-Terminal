4d2a487d19ebb4e1801a5857ca5e317246c56bce
Vex87 <AndrewKyleGuilas@gmail.com>
2021-04-23 23:13:37 -0400

    updated help cmd

--- constants.py
+++ constants.py
@@ -1,16 +1,18 @@
 commands = """
-    help                Retrieves commands
+    help                        Retrieves commands
     set
-        guild           Sets the selected guild.
-        guildname       Sets the guild's name. Must be the guild name or id.
-        guildicon       Sets the guild's icon. Must be a URL.
-        afkchannel      Sets the timeout channel. Must be the channel name or id.
-        afktimeout      Sets the timeout duration.
+        guild                   Sets the selected guild. Must be the guild name or id.
+        guildname               Sets the guild's name.
+        guildicon               Sets the guild's icon. Must be a URL.
+        afkchannel              Sets the timeout channel. Must be the channel name or id.
+        afktimeout              Sets the timeout duration in seconds.
+        defaultnotifications    Sets the guild's default notifications level. Must be allmessages or onlymentions.
     get
-        guilds          Retrieves the guilds the bot is connected to.
-        guild           Retrieves the selected guild.
-        guildname       Retrieves the guild's name.
-        guildicon       Retrieves the guild's icon as a URL.
-        afkchannel      Retrieves the guild's timeout channel.
-        afktimeout      Retrieves the guild's timeout duration.
+        guilds                  Retrieves the guilds the bot is connected to.
+        guild                   Retrieves the selected guild.
+        guildname               Retrieves the guild's name.
+        guildicon               Retrieves the guild's icon as a URL.
+        afkchannel              Retrieves the guild's timeout channel.
+        afktimeout              Retrieves the guild's timeout duration.
+        defaultnotifications    Retrieves the guild's default notifications level.
 """
